69b437a4892642357de07be45536840308788a02
DENIS Victor <victordenis01@gmail.com>
2022-01-07 10:40:52 +0100

    🎨 Changement de la gestion de la navigation

--- lib/core/main_page/main_page.dart
+++ lib/core/main_page/main_page.dart
@@ -1,23 +1,34 @@
 import 'package:chemin_du_local/core/helpers/screen_helper.dart';
 import 'package:chemin_du_local/core/main_page/page_item.dart';
+import 'package:chemin_du_local/core/main_page/widgets/bottom_app_bar.dart' as custom;
 import 'package:chemin_du_local/core/main_page/widgets/menu_drawer.dart';
 import 'package:chemin_du_local/features/dashboard/dashboard_page.dart';
+import 'package:chemin_du_local/features/products/products_controller.dart';
+import 'package:chemin_du_local/features/products/products_main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 const Map<PagesId, PageItem> pagesItem = {
     PagesId.dashboard: PageItem(id: PagesId.dashboard, title: "Accueil", appBarTitle: "Bienvenue Guillaume", icon: Icons.home),
     PagesId.products: PageItem(id: PagesId.products, title: "Mes Produits", appBarTitle: "Mes Produits", icon: Icons.shopping_basket),
 };
 
-class MainPage extends StatefulWidget {
+class MainPage extends ConsumerStatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
-  State<MainPage> createState() => _MainPageState();
+  ConsumerState<MainPage> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends ConsumerState<MainPage> {
   PagesId _currentPageId = PagesId.dashboard;
+
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(productsControllerProvider.notifier).loadData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,42 +40,79 @@ class _MainPageState extends State<MainPage> {
           });
         }
       ),
-      PagesId.products: const Center(child: Text("Hello page 2")),
+      PagesId.products: const ProductsMainPage()
     };
 
     return LayoutBuilder(
       builder: (context, constraints) {
         bool useBigLayout = constraints.maxWidth >= ScreenHelper.breakpointPC;
 
-        return Row(
+        return Column(
           mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            if (useBigLayout)
-              MenuDrawer(
-                onSelectedPage: (pageId) {
-                  setState(() {
-                    _currentPageId = pageId;
-                  });
-                }, 
-                shouldPop: false, 
-                currentPageId: _currentPageId
-              ),
             Expanded(
-              child: Scaffold(
-                appBar: AppBar(title: Text(pagesItem[_currentPageId]?.appBarTitle ?? ""),),
-                drawer: useBigLayout ? null : MenuDrawer(
-                  onSelectedPage: (pageId) {
-                    setState(() {
-                      _currentPageId = pageId;
-                    });
-                  },
-                  shouldPop: true,
-                  currentPageId: _currentPageId,
-                ),
-                body: pages[_currentPageId],
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  if (useBigLayout)
+                    MenuDrawer(
+                      onSelectedPage: (pageId) {
+                        setState(() {
+                          _currentPageId = pageId;
+                        });
+                      }, 
+                      shouldPop: false, 
+                      currentPageId: _currentPageId
+                    ),
+                  Expanded(
+                    child: Navigator(
+                      onGenerateRoute: (route) => MaterialPageRoute<dynamic>(
+                        settings: route,
+                        builder: (context) => Scaffold(
+                          appBar: AppBar(title: Text(pagesItem[_currentPageId]?.appBarTitle ?? ""),),
+                          body: pages[_currentPageId],
+                        )
+                      ),
+                    ),
+                    // child: Scaffold(
+                    //   appBar: AppBar(title: Text(pagesItem[_currentPageId]?.appBarTitle ?? ""),),
+                    //   // drawer: useBigLayout ? null : MenuDrawer(
+                    //   //   onSelectedPage: (pageId) {
+                    //   //     setState(() {
+                    //   //       _currentPageId = pageId;
+                    //   //     });
+                    //   //   },
+                    //   //   shouldPop: true,
+                    //   //   currentPageId: _currentPageId,
+                    //   // ),
+                    //   body: pages[_currentPageId],
+                    // ),
+                  ),
+                ],
               ),
             ),
+            if (!useBigLayout)
+              Container(
+                height: 64,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                  border: const Border(
+                    top: BorderSide(
+                      color: Color(0xffd9d9d9),
+                      width: 0.5
+                    )
+                  )
+                ),
+                child: custom.BottomAppBar(
+                  currentPageId: _currentPageId,
+                  onSelectedPage: (page) {
+                    setState(() {
+                      _currentPageId = page;
+                    });
+                  },
+                ),
+              )
           ],
         );
       }
---
+++ lib/core/main_page/widgets/bottom_app_bar.dart
@@ -0,0 +1,51 @@
+import 'package:chemin_du_local/core/main_page/main_page.dart';
+import 'package:chemin_du_local/core/main_page/page_item.dart';
+import 'package:chemin_du_local/theme/palette.dart';
+import 'package:flutter/material.dart';
+
+class BottomAppBar extends StatelessWidget {
+  const BottomAppBar({
+    Key? key,
+    required this.onSelectedPage,
+    required this.currentPageId
+  }) : super(key: key);
+
+  final Function(PagesId) onSelectedPage;
+  final PagesId currentPageId;
+
+  final List<PagesId> pageIds = const [
+    PagesId.dashboard, 
+    PagesId.products
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: [
+        _buildItem(
+          icon: pagesItem[PagesId.dashboard]!.icon,
+          label: pagesItem[PagesId.dashboard]!.title
+        ),
+        _buildItem(
+          icon: pagesItem[PagesId.products]!.icon,
+          label: pagesItem[PagesId.products]!.title
+        ),
+      ],
+      unselectedItemColor: Palette.colorDark,
+      selectedItemColor: Palette.colorPrimary,
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      currentIndex: pageIds.indexOf(currentPageId),
+      onTap: (index) => onSelectedPage(pageIds[index]),
+    );
+  }
+
+  BottomNavigationBarItem _buildItem({
+    required IconData icon,
+    required String label,
+  }) {
+    return BottomNavigationBarItem(
+      icon: Icon(icon),
+      label: label
+    );
+  }
+}
