469fd49cb6cd813d3ff2ab843da1dbc9c6335bc4
Feldrise <admin@feldrise.com>
2022-03-31 14:54:42 +0200

    💄 Amélioration de la bar en mobile

--- lib/features/main_page/widgets/bottom_app_bar_painter.dart
+++ lib/features/main_page/widgets/bottom_app_bar_painter.dart
@@ -13,13 +13,13 @@ class BottomAppBarPainter extends CustomPainter {
     path.moveTo(0, 0);
     path.lineTo(size.width * 0.35, 0);
     // path.quadraticBezierTo(size.width * 0.20, 0, size.width * 0.35, 0);
-    path.quadraticBezierTo(size.width * 0.4, 0, size.width * 0.4, 20);
+    path.quadraticBezierTo(size.width * 0.5 - 40, 0, size.width * 0.5 - 40, 20);
     path.arcToPoint(
-      Offset(size.width * 0.6, 20),
+      Offset(size.width * 0.5 + 40, 30),
       radius: const Radius.circular(10),
       clockwise: false
     );
-    path.quadraticBezierTo(size.width * 0.6, 0, size.width * 0.65, 0);
+    path.quadraticBezierTo(size.width * 0.5 + 40, 0, size.width * 0.55 + 40, 0);
     path.lineTo(size.width, 0);
     path.lineTo(size.width, size.height);
     path.lineTo(0, size.height);
