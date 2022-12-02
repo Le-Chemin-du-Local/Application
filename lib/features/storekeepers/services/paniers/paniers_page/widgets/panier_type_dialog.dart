6f3e68d4dbc10cdd9ec2c581c38023d7fa781145
Feldrise <admin@feldrise.com>
2022-12-02 10:18:29 +0100

    📱 Correction de l'affichage lors de l'ajout de paniers

--- lib/features/storekeepers/services/paniers/paniers_page/widgets/panier_type_dialog.dart
+++ lib/features/storekeepers/services/paniers/paniers_page/widgets/panier_type_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:chemin_du_local/core/helpers/screen_helper.dart';
 import 'package:chemin_du_local/core/widgets/dialog/closable_dialog.dart';
 import 'package:chemin_du_local/features/storekeepers/services/paniers/models/panier/panier.dart';
 import 'package:chemin_du_local/presentation/c_l_icons_icons.dart';
@@ -13,67 +14,81 @@ class PanierTypeDialog extends StatelessWidget {
       title: "Quel est le type de panier ?",
       child: DefaultTextStyle(
         style: const TextStyle(color: Palette.colorWhite),
-        child: Row(
-          children: [
-            // Panier flash
-            Flexible(
-              child: InkWell(
-                onTap: () => Navigator.of(context).pop(PanierType.temporary),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
-                    borderRadius: BorderRadius.circular(12)
-                  ),
-                  padding: const EdgeInsets.all(40),
-                  child: Center(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          CLIcons.clickAndCollect,
-                          size: 92,
-                          color: Palette.colorWhite,
-                        ),
-                        SizedBox(height: 10,),
-                        Text("Créer un panier flash")
-                      ],
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final List<Widget> children = [
+              // Panier flash
+              Flexible(
+                child: InkWell(
+                  onTap: () => Navigator.of(context).pop(PanierType.temporary),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                    padding: const EdgeInsets.all(40),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Icon(
+                            CLIcons.clickAndCollect,
+                            size: 92,
+                            color: Palette.colorWhite,
+                          ),
+                          SizedBox(height: 10,),
+                          Text("Créer un panier flash")
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(width: 12,),
-      
-            // Panier permanant
-            Flexible(
-              child: InkWell(
-                onTap: () => Navigator.of(context).pop(PanierType.permanent),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
-                    borderRadius: BorderRadius.circular(12)
-                  ),
-                  padding: const EdgeInsets.all(40),
-                  child: Center(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          CLIcons.clickAndCollect,
-                          size: 92,
-                          color: Palette.colorWhite,
-                        ),
-                        SizedBox(height: 10,),
-                        Text("Créer un panier permanant")
-                      ],
+              const SizedBox(width: 12, height: 12,),
+    
+              // Panier permanant
+              Flexible(
+                child: InkWell(
+                  onTap: () => Navigator.of(context).pop(PanierType.permanent),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                    padding: const EdgeInsets.all(40),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Icon(
+                            CLIcons.clickAndCollect,
+                            size: 92,
+                            color: Palette.colorWhite,
+                          ),
+                          SizedBox(height: 10,),
+                          Text("Créer un panier permanant")
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ];
+
+            if (constraints.maxWidth >= ScreenHelper.breakpointTablet) {
+              return Row(
+                children: children,
+              );
+            }
+
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: children,
+            );
+          }
         ),
       )
     );
